375e2bdbbf194bf9a75dd02c5b275360833e7040
js <89633614+icodeunot@users.noreply.github.com>
2022-05-24 02:51:09 -0700

    Create degree.h

---
+++ degree.h
@@ -0,0 +1,10 @@
+#ifndef DEGREE_H
+#define DEGREE_H
+#include <iostream>
+
+	// define enumerated data type for degree programs
+enum DegreeProgram {SECURITY, NETWORK, SOFTWARE};
+
+static const std::string degreeProgramStrings[] = {"SECURITY","NETWORK","SOFTWARE"};
+
+#endif
